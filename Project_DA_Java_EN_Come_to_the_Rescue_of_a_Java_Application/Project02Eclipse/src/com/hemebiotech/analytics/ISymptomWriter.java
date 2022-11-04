5d662d9b616841bbfb4c0441f5229047e98dcd83
joselct17 <62754339+joselct17@users.noreply.github.com>
2022-11-04 09:53:12 +0100

    comment javadoc

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -2,15 +2,18 @@ package com.hemebiotech.analytics;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.TreeMap;
+
 
 /**
+ * Anything that will write symptom data in an out file
+ * The important part is, the return value from the operation, which is a treeMap,
+ * that may contain many duplications
  *
  */
 public interface ISymptomWriter {
     /**
      *
-     * @param treeMap
+     * @param treeMap a map with symptoms as key and number of occurrences
      * @return a map with all symptoms one per line
      */
 
